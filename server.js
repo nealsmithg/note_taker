2c04c1d1e43bc736cc0ff551f0788e757524631e
Cornelius Smith <nealsmithg@gmail.com>
2021-10-18 16:54:16 -0400

    added paths for base website

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const html = require("./routes/htmlRoutes");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
