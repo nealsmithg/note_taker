02eda86b73d0fcec4d2cc91b063ff1d735dd8fee
Cornelius Smith <nealsmithg@gmail.com>
2021-10-19 12:48:45 -0400

    added api routes

--- db/store.js
+++ db/store.js
@@ -3,4 +3,35 @@ const util = require("util");
 
 const readFromFile = util.promisify(fs.readFile);
 
-module.exports = { readFromFile}
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        fs.writeFile(file, JSON.stringify(parsedData, null, 4), (err)=>
+        err ? console.log(err) : console.log("new note writen"));
+      }
+    });
+  };
+
+const readAndDelete = (id, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            for (i=0; i<parsedData.length; i++){
+                if(parsedData[i].id ===id){
+                    parsedData.splice(i,1);
+
+                    fs.writeFile(file, JSON.stringify(parsedData, null, 4), (err)=>
+                    err ? console.log(err) : console.log("file deleted"));
+                }
+            }
+        };
+    });
+};
+
+module.exports = { readFromFile, readAndAppend, readAndDelete}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,32 @@
 const path = require("path");
-const { readFromFile} = require("../db/store");
+const { readFromFile, readAndAppend, readAndDelete} = require("../db/store");
 const router = require("express").Router();
+const { v4: uuidv4 } = require('uuid');
 
 
 router.get("/notes", (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+router.post("/notes", (req, res) => {
+    console.log(req.body);
+    console.log(uuidv4());
+    const {title, text} = req.body;
+    if (req.body){
+        const newNote = {
+            title,
+            text,
+            id: uuidv4()
+        };
+
+        readAndAppend(newNote, "./db/db.json");
+        readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    }
+});
+
+router.delete("/notes/:id", (req, res) => {
+    const {id} = req.params;
+    readAndDelete(id, "./db/db.json");
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
